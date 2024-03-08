b41f5d8020e1086f46d94d3e3233444ce85f3aab
cyberskeleton <62664421+cyberskeleton@users.noreply.github.com>
2024-03-08 19:25:20 +0200

    Delete lab02_FactoryMethod directory

--- lab02_FactoryMethod/task_3_1/src/LogisticsAmericano.java
+++
@@ -1,6 +0,0 @@
-public class LogisticsAmericano extends Logistics {
-    @Override
-    protected Coffee makeCoffee(float beansCost, float waterCost, float sugarCost, float milkCost) {
-        return new CoffeeAmericano(beansCost, waterCost, sugarCost);
-    }
-}
--- lab02_FactoryMethod/task_3_1/src/LogisticsEspresso.java
+++
@@ -1,6 +0,0 @@
-public class LogisticsEspresso extends Logistics {
-    @Override
-    protected Coffee makeCoffee(float beansCost, float waterCost, float sugarCost, float milkCost) {
-        return new CoffeeEspresso(beansCost, waterCost, sugarCost);
-    }
-}
